223b1c93c63b92e79058a0023ce62ec073ea2908
vijayjohnasirvatham-star <vijayjohnasirvatham@gmail.com>
2026-03-31 05:33:25 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,24 +1,36 @@
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+
+-- Customers Table
 CREATE TABLE customers (
     id INT PRIMARY KEY,
     name VARCHAR(50),
     city VARCHAR(50)
 );
 
+-- Products Table
 CREATE TABLE products (
     id INT PRIMARY KEY,
     product_name VARCHAR(50),
     price DECIMAL(10,2)
 );
 
+-- Orders Table
 CREATE TABLE orders (
     id INT PRIMARY KEY,
     customer_id INT,
-    order_date DATE
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
+-- Order Items Table
 CREATE TABLE order_items (
     id INT PRIMARY KEY,
     order_id INT,
     product_id INT,
-    quantity INT
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
